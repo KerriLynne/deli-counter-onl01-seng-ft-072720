ebf2460e5e8ddc4aaac7d21bcc7d89127c20f245
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-29 20:46:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,18 +15,15 @@ def line(array)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  counter = 1
-    katz_deli.each_with_index do |name|
-    puts "Welcome, #{name}. You are number #{counter} in line."
-    counter += 1
-  end
+    katz_deli << (name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
 def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
   puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
-end
+  end
 end
