0df79e76368fab5c53e7d9f3170d1ea710e32762
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-29 20:31:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,15 @@
 # Write your code here.
 katz_deli = []
-
 def line(array)
   line_position = []
   if array.empty?
-   puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     counter = 1
     array.each do |customer|
       line_position << "#{counter}. #{customer}"
       counter += 1
     end
-    puts "The line is currently: #{line_position.join*" "}"
+    puts "The line is currently: #{line_position.join(" ")}"
   end
 end
