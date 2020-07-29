4ccffb0f33e246eb9e840b2e76de234634617234
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-29 20:39:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,20 @@ def line(array)
     puts "The line is currently: #{line_position.join(" ")}"
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  counter = 1
+    katz_deli.each_with_index do |name|
+    puts "Welcome, #{name}. You are number #{counter} in line."
+    counter += 1
+  end
+end
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift[0]
+end
+end
